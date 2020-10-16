57fd05d043ae955c01ef3105539d93291eb3b742
gabrielsallesmota <gabrielsallesmota@hotmail.com>
2020-10-15 22:40:24 -0300

    Add files via upload

---
+++ JavaScript/Curso Web Moderno - 2020/Fundamentos/usandoVar1.js
@@ -0,0 +1,23 @@
+{ 
+    { 
+        { 
+            { 
+                var sera = 'Sera????' 
+                console.log(sera)
+            } 
+        } 
+    }
+}     
+
+console.log(sera)
+
+function teste(){
+    var local = 123
+    console.log (local)
+}
+
+teste ()
+
+//quando se define uma variavel dentro da função,ele estará diponível apenas no escopo da função;
+//se deve evitar criar uma variavel no escopo global por qualquer sobscrita sobre a variável ele aceita;
+// variavel existe dois tipos, ou variavel global ou então variavel no escopo de função (que fica dentro de uma função)
---
+++ JavaScript/Curso Web Moderno - 2020/Fundamentos/usandoVarEmLoop1.js
@@ -0,0 +1,8 @@
+//primeira parte: declaração da variavel
+//segunda: condição para ficar repetindo
+//terceiro: incremento
+
+for (var i=0; i<10; i++) {
+    console.log (i)
+}
+console.log('i=', i)
